19f1382d4c062430da44a683ef1cb09eccd9d05d
David Felix <davidffelix@hotmail.com>
2022-04-12 17:58:32 -0300

    Create script

---
+++ script.js
@@ -0,0 +1,18 @@
+function start() {
+    console.log("Iniciou");
+}
+
+
+function pause() {
+    console.log("Iniciou");
+}
+
+
+function stop() {
+    console.log("Iniciou");
+}
+
+
+function counter() {
+    console.log
+}
